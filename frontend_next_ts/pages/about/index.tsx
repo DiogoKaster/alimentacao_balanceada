81b5dc821c0ee25095c59c1c0911a0ecaed50a84
CacaAlves00 <cacazinho99@gmail.com>
2023-01-11 16:43:40 -0300

    AboutPage converted to ts

---
+++ frontend_next_ts/pages/about/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import styles from './../../styles/AboutPage.module.scss'
+import Image from 'next/image'
+
+function AboutPage() {
+    return (
+        <article id={styles['about']}>
+            <header>
+                <Image
+                    src='/imgs/dse.png'
+                    alt='Desenvolvimento de Software Educacional'
+                    width={500}
+                    height={500}
+                />
+
+                <h1>Sobre</h1>
+
+                <Image
+                    src='/imgs/lesic.png'
+                    alt='LESIC'
+                    width={500}
+                    height={500}
+                />
+            </header>
+
+            <main>
+                <p>Universidade Tecnológica Federal Do Paraná</p>
+                <p>Aluno: Antônio Carlos Alves Feitosa</p>
+                <p>Professora: Simone Nasser Matos</p>
+                <p>E-mail: antoniofeitosa@alunos.utfpr.edu.br</p>
+                <p>GitHub: https://github.com/CacaAlves00</p>
+            </main>
+
+        </article>
+    )
+}
+
+export default AboutPage
